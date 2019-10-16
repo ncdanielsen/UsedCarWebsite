57945cdf4b11243f54f878b197a5f9d13ec886a1
Nils <ncdanielsen@live.co.uk>
2019-10-16 18:03:11 +0200

    Initial Commit

---
+++ UsedCarWebsite.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using UsedCarWebsite.API.Data;
+
+namespace UsedCarWebsite.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly DataContext context;
+        public WeatherForecastController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+    }
+}
